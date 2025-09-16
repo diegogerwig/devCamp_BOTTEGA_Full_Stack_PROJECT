77f12f1c0979a267abf2389f18a72c8e40cd7bdb
Diego Gerwig <diegogerwig@gmail.com>
2025-09-16 22:31:59 +0200

    refresh

--- backend/app.py
+++ backend/app.py
@@ -384,6 +384,7 @@ def init_database():
 # Inicializar
 init_database()
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
